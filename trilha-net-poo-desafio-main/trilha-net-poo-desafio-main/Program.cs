eeb7bdafe66f759182bbe4d8ddb14e5340a17421
Felipe Cruz <160171970+madokanomi@users.noreply.github.com>
2024-10-30 01:14:37 -0300

    Add files via upload

---
+++ trilha-net-poo-desafio-main/trilha-net-poo-desafio-main/Program.cs
@@ -0,0 +1,13 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero:"12345", modelo: "Modelo 10", imei:"11111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+Smartphone iphone = new Iphone(numero:"123144", modelo: "Modelo 13", imei:"2222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
